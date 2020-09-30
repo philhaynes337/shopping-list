f177181b7f40721de575ef5c79c5862e74c953b9
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2020-09-29 19:41:19 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -8,6 +8,19 @@ $(function() {
 
 			$('#shopping-list-entry').val('');
 
+			    $('.shopping-list').append(
+      `<li>
+        <span class="shopping-item">${listItem}</span>
+        <div class="shopping-item-controls">
+          <button class="shopping-item-toggle">
+            <span class="button-label">check</span>
+          </button>
+          <button class="shopping-item-delete">
+            <span class="button-label">delete</span>
+          </button>
+        </div>
+      </li>`);
+
 	})
 }
 
