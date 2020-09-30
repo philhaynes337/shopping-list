0656d4d934a0005d266bbfdc4d0e9ae50b575cf4
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2020-09-29 19:57:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -20,12 +20,6 @@ $(function(){
       </li>`);
   });
 
-  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-    $(this).closest('li').remove();
-  });
 
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-  });
 
 });
