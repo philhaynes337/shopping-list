02a7eba714496fc8ec47e21ed6bd30ebc5713f1e
philhaynes337 <71613755+philhaynes337@users.noreply.github.com>
2020-09-29 19:46:03 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $(function() {
 	$('#js-shopping-list-form').submit(event => {
 
 
-		const addItem = $('.js-shopping-list-entry').val();
+		const addItem = $('.shopping-list-entry').val();
 
 			$('#shopping-list-entry').val('');
 
